4e0041b1643bb8aee3cd328e54b05c223d8cbe2a
misha <44443489+mishaarefa@users.noreply.github.com>
2022-08-22 15:19:26 +0300

    Add files via upload

---
+++ mishaa.py
@@ -0,0 +1,31 @@
+
+a = (3)
+b = (3)
+c = (3)
+
+print(id(a))
+print(id(b))
+print(id(c))
+
+d = [1]
+e = [1]
+
+print( )
+print(id(d))
+print(id(e))
+print( )
+
+a = [3]
+b = [3]
+c = [3]
+
+print(id(a))
+print(id(b))
+print(id(c))
+
+d = (1)
+e = (1)
+
+print( )
+print(id(d))
+print(id(e))
