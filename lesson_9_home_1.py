7f051d2d97cd52e0d3f41ee2c2cdb88c86b456c3
EvgeniyArefa <areinna@i.ua>
2023-01-01 23:18:08 +0200

    jj

--- lesson_9_home_1.py
+++ lesson_9_home_1.py
@@ -1,18 +1,17 @@
 
-
 def func(val):
     ret = ""
     val = val.replace(",", ".")
-    if not val.replace("-", "").replace(".", "").strip().isdigit():
+    if not val.replace("-", "", 1).replace(".", "", 1).strip().isdigit():
         ret = "Вы ввели не корректное число: " + val
     else:
         if "-" in val:
             ret = "Вы ввели отрицательное "
-        elif "-" not in val:
+        if "-" not in val:
             ret = "Вы ввели положительное "
         if "." not in val:
             ret += "целое число: "
-        elif "." in val:
+        if "." in val:
             ret += "дробное число: "
         ret += val
     return ret
@@ -20,7 +19,8 @@ def func(val):
 
 while True:
     value = input("Введите число либо значение для выхода: ")
-    if value == "выход":
+    e = value.lower()
+    if e == "выход" or e == "exit" or e == "quit" or e == "e" or e == "q":
         break
     else:
         print(func(value))
