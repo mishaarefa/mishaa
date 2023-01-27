7ea62968cb67d10590388c3a5c6e95388857b24d
EvgeniyArefa <areinna@i.ua>
2023-01-27 19:26:14 +0200

    jj

--- lesson_11_home_2.py
+++ lesson_11_home_2.py
@@ -1,4 +1,5 @@
 
+import random
 import csv
 import json
 
@@ -6,29 +7,29 @@ with open('names.json') as f:
     names = json.load(f)
 f.close()
 
-print(names)
-column = [" "]
-for x in range(1, len(names.values())+1):
-    column.append("Person" + " " + str(x))
+column = ["id", "name", "age", "phone"]
+persons = []
 
+for x, y in enumerate(names):
+    persons.append([y])
+    for z in names.get(y):
+        persons[x].append(z)
 
-ids = ["id"]
-name = ["name"]
-age = ["age"]
-phone = ["phone", "097-09-32", "096-92-85", "095-34-54", "094-93-85", "090-43-53"]
+operators = ["039", "067", "068", "096", "097", "098"]
 
-for x in names.keys():
-    ids.append(str(x))
+list1 = []
+for x in range(0, 10):
+    list1.append(x)
 
-for x in list(names.values()):
-    name.append(str(x[0]))
-
-for x in list(names.values()):
-    age.append(str(x[1]))
-
+for x in range(len(persons)):
+    r = "+38" + random.choice(operators)
+    for y in range(7):
+        r += str(random.choice(list1))
+    persons[x].append(r)
 
 with open('names.csv', 'w', newline='', encoding='utf-8') as f:
     writer = csv.writer(f)
-    for x in (column, ids, name, phone, age):
+    writer.writerow(column)
+    for x in persons:
         writer.writerow(x)
 f.close()
