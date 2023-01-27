c763f6f4d1fbac614a3e449a2191b2906a4221b9
EvgeniyArefa <areinna@i.ua>
2023-01-27 22:31:42 +0200

    jj

--- lesson_11_home_3.py
+++ lesson_11_home_3.py
@@ -7,16 +7,36 @@ with open("names.csv", encoding='utf-8') as f:
     data = list(reader)
 f.close()
 
+for x, y in enumerate(data):
+    data[x].pop(2)
+data.pop(0)
+
+tele = ["phone"]
+ids = ["id"]
+names = ["name"]
+person = [" "]
+
+for m in range(len(data)):
+    person.append("Person"+" "+str(m+1))
+
 for x in data:
-    if x[0] == "age":
-        data.remove(x)
+    if len(x) == 2:
+        tele.append("")
+    for y in x:
+        y.rstrip()
+        if y.startswith("+380"):
+            tele.append(y)
+        elif y.isdigit():
+            ids.append(y)
+        else:
+            names.append(y)
 
 wb = openpyxl.Workbook()
-sheet = wb['Sheet']
+sheet = wb["Sheet"]
 
-for x in data:
-    for y in x:
-        cell = sheet.cell(row=int(data.index(x))+1, column=int(x.index(y))+1)
-        cell.value = y
+for x, y in enumerate((person, ids, names, tele)):
+    for z, a in enumerate(y):
+        cell = sheet.cell(row=x+1, column=z+1)
+        cell.value = a
 
 wb.save("Names1.xlsx")
